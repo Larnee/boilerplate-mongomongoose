7e0d2ff4b95f0f76b2865a9137449e5e4092ee35
larnee <larnee@mailfence.com>
2022-06-22 01:30:28 +0200

    Solution 2.2

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new mongoose.Schema({
-	name: String,
+	name: { type: String, required: true },
 	age: Number,
-	favoriteFoods: Array
+	favoriteFoods: [String]
 })
 
 module.exports = mongoose.model('Person', personSchema);
