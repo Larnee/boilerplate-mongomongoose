bfda0e8632a2bd40b607355bde86dc7170a5d74b
larnee <larnee@mailfence.com>
2022-06-23 00:09:20 +0200

    Solution 12.5

--- myApp.js
+++ myApp.js
@@ -101,7 +101,7 @@ const queryChain = (done) => {
 	Person.find({favoriteFoods:foodToSearch})
 				.sort({name: 1})
 				.limit(2)
-				.select({name: 1,age:0,favoriteFoods:1})
+				.select({age:0})
 				.exec(function(err, data){
 					if(err) return console.log(err);
   				done(null, data);
